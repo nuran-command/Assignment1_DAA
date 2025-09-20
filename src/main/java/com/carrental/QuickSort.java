ae51a0b703b70b381a1a293e36a9be806a3ae017
Nurdan Zhanabaev <nurdanzanabaev@gmail.com>
2025-09-20 17:28:13 +0500

    feat(quicksort): add quicksort + tests

--- src/main/java/com/carrental/QuickSort.java
+++ src/main/java/com/carrental/QuickSort.java
@@ -1,4 +1,53 @@
 package com.carrental;
 
 public class QuickSort {
+    private static final int CUTOFF = 16; // switch to insertion sort if n < 16
+
+    public static void sort(int[] arr) {
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    private static void quickSort(int[] arr, int low, int high) {
+        if (high - low < CUTOFF) {
+            insertionSort(arr, low, high);
+            return;
+        }
+
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high]; // choose last element as pivot
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    private static void insertionSort(int[] arr, int low, int high) {
+        for (int i = low + 1; i <= high; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= low && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
 }
--- src/test/java/QuickSortTest.java
+++ src/test/java/QuickSortTest.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import com.carrental.QuickSort;
 
 public class QuickSortTest {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 1, 5, 6};
+        QuickSort.sort(arr);
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+    }
 }
