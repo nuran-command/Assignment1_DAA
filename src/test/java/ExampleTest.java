fdb285436573acefd210409f2590d48ef3e062d7
Nurdan Zhanabaev <nurdanzanabaev@gmail.com>
2025-09-20 15:25:35 +0500

    init: maven, junit5, ci, readme

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+    @Test
+    void simpleTest() {
+        assertEquals(4, 2 + 2);
+    }
+}
